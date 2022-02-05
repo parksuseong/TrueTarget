e5fce65030c1b0f58d3098f12eb1e28fcd9b4a07
parksuseong <parksuseong@gmail.com>
2022-02-05 18:42:53 +0900

    first commit

---
+++ TelegramModel.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import time
+import telegram
+
+class TelegramModel():
+    
+    def __init__(self):
+        self.chat_token = "5294332221:AAEllq5Bw4CSFa3WzP1VB-5HfaNziKk7ypc"
+        self.bot = telegram.Bot(token = self.chat_token)
+        self.user_lst = []
+    
+    def get_userupdate(self): #유저 업데이트 하는 함수
+        updates = self.bot.getUpdates()
+        user_set = set()
+        for u in updates:
+            user_set.add(u.message['chat']['id'])
+            self.user_lst = list(user_set)
+
+    def send_message(self,text): #메세지 보내는 함수
+        self.get_userupdate()
+        for usr in self.user_lst:
+            self.bot.sendMessage(chat_id = usr, text=text)
