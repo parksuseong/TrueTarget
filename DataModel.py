fd6f177f501c56662ef83024af95906bdb2452c0
parksuseong <godsuseong@gmail.com>
2022-06-06 23:26:39 +0900

    test

--- DataModel.py
+++ DataModel.py
@@ -1,27 +1,74 @@
+
 import ccxt
 from datetime import datetime, timedelta
 import pandas as pd
 
 
+"""
+일봉 = 4시간봉 * 6 = 1시간봉 * 96 = 30분봉 * 192 = 15분봉 * 384 = 5분봉 * 1152
+"""
 
 class DataModel():
     
-    def __init__(self,market,periods):
+    def __init__(self,market,periods,from_date,to_date):
         self.exchange = ccxt.binance()
         self.market = market
         self.periods = periods
+        self.df = pd.DataFrame([], columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+        self.get_data(from_date=from_date,to_date=to_date)
+        self.calc_rsi()
         
-        self.get_data()
+    def get_data(self,from_date=None,to_date=None):
+        to_date = int(pd.to_datetime(to_date).timestamp()*1000)
+        from_date = int(pd.to_datetime(from_date).timestamp()*1000)
+        
+        while(from_date <= to_date):
+            ohlcvs = self.exchange.fetch_ohlcv(self.market, self.periods, since=from_date, limit=1000)
+            tmp_df = pd.DataFrame(ohlcvs, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])   
+            self.df = pd.concat([self.df,tmp_df])
+            from_date = int(tmp_df['datetime'][-1:])+1
 
         
-    def get_data(self):
-        self.ohlcvs = self.exchange.fetch_ohlcv(self.market, self.periods)
-        #일자, 시가, 고가, 저가, 종가, 거래량
-        self.df = pd.DataFrame(self.ohlcvs, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+        del_idx = self.df[ self.df['datetime'] >= to_date].index
+        self.df.drop(del_idx,inplace=True)
+        
         self.df['datetime'] = pd.to_datetime(self.df['datetime'], unit='ms')
-        self.df['datetime'] = pd.DatetimeIndex(self.df['datetime']) + timedelta(hours=9)
+        self.df['datetime'] = pd.DatetimeIndex(self.df['datetime']) #+ timedelta(hours=9)
         self.df.set_index('datetime', inplace=True)
-        self.df.reset_index(inplace=True)
+        self.df.reset_index(inplace=True)  
         return self.df
 
-    
+
+        
+
+    #calculate rsi
+    def calc_rsi(self, periods=14, ema=True):
+        close_delta=self.df['close'].diff()
+        up=close_delta.clip(lower=0)
+        down=-1*close_delta.clip(upper=0)
+
+        if ema: #exponentail moving avg
+            ma_up=up.ewm(com=periods-1, adjust=True, min_periods=periods).mean()
+            ma_down=down.ewm(com=periods-1, adjust=True, min_periods=periods).mean()
+        else: #simple moving avg
+            ma_up=up.rolling(window.periods).mean()
+            ma_down=up.rolling(window.periods).mean()
+
+        rsi=ma_up/ma_down
+        rsi=100-(100/(1+rsi))
+        rsi.fillna(0,inplace=True)
+        rsi = rsi.to_frame()
+        rsi.columns = ['rsi']
+        
+        self.df = pd.concat([self.df,rsi], axis=1, join='inner')
+        
+        #result = pd.DataFrame(result, columns=['datetime', 'open','high','low','close','vol','rsi'])
+        self.df.columns = ['datetime','open','high','low','close','volume','rsi']
+
+        del_idx = self.df[ self.df['rsi'] == 0].index
+        self.df.drop(del_idx,inplace=True)
+
+        return self.df
+
+    def get_df(self):
+        return self.df
--- main.py
+++ main.py
@@ -3,18 +3,104 @@ import DataModel
 import matplotlib as mpl
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Conv1D, Lambda
+from tensorflow.keras.losses import Huber
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import os
+import datetime
+import numpy as np
 
 
-model = DataModel.DataModel('BTC/USDT','1d')
-df = model.get_data()
+model = DataModel.DataModel('BTC/USDT',periods='15m',from_date='2020-01-01',to_date='2022-06-06')
+df = model.get_df()
 print(df)
 
+scaler = MinMaxScaler()
+scale_cols = ['open', 'high', 'low', 'close', 'volume', 'rsi']
+scaled = scaler.fit_transform(df[scale_cols])
+df = pd.DataFrame(scaled, columns=scale_cols)
 
-#df[["close"]].plot()
-#plt.show()
+x_train, x_test, y_train, y_test = train_test_split(df.drop('close', 1), df['close'], test_size=0.1, random_state=0, shuffle=False)
+x_train.shape, y_train.shape
+print(type(x_test))
+print(x_test)
+
+
+def windowed_dataset(x, y, window_size, batch_size, shuffle):
+    # X값 window dataset 구성
+    ds_x = tf.data.Dataset.from_tensor_slices(x)
+    ds_x = ds_x.window(window_size, shift=1, stride=1, drop_remainder=True)
+    ds_x = ds_x.flat_map(lambda x: x.batch(window_size))
+    # y값 추가
+    ds_y = tf.data.Dataset.from_tensor_slices(y[window_size:])
+    ds = tf.data.Dataset.zip((ds_x, ds_y))
+    if shuffle:
+        ds = ds.shuffle(1000)
+    return ds.batch(batch_size).prefetch(1)
+
+WINDOW_SIZE=5
+BATCH_SIZE=32
+
+train_data = windowed_dataset(x_train, y_train, WINDOW_SIZE, BATCH_SIZE, True)
+test_data = windowed_dataset(x_test, y_test, WINDOW_SIZE, BATCH_SIZE, False)
+
+model = Sequential([
+    # 1차원 feature map 생성
+    Conv1D(filters=32, kernel_size=5,
+           padding="causal",
+           activation="relu",
+           input_shape=[WINDOW_SIZE, 5]),
+    # LSTM
+    LSTM(16, activation='tanh'),
+    Dense(16, activation="relu"),
+    Dense(1),
+])
+
+# Sequence 학습에 비교적 좋은 퍼포먼스를 내는 Huber()를 사용합니다.
+loss = Huber()
+optimizer = Adam(0.0005)
+model.compile(loss=Huber(), optimizer=optimizer, metrics=['mse'])
+
+# earlystopping은 35번 epoch통안 val_loss 개선이 없다면 학습을 멈춥니다.
+earlystopping = EarlyStopping(monitor='val_loss', patience=35)
+# val_loss 기준 체크포인터도 생성합니다.
+filename = os.path.join('tmp', 'ckeckpointer.ckpt')
+checkpoint = ModelCheckpoint(filename, 
+                             save_weights_only=True, 
+                             save_best_only=True, 
+                             monitor='val_loss', 
+                             verbose=1)
+                             
+history = model.fit(train_data, 
+                    validation_data=(test_data), 
+                    epochs=1000, 
+                    callbacks=[checkpoint, earlystopping])
+
+
+print("%.7f" % (float(min(history.history['val_loss']))))
+model.load_weights(filename)
+pred = model.predict(test_data)
+actual = np.asarray(y_test)[WINDOW_SIZE:]
+actual = np.reshape(actual, (len(actual), 1))
+print(pred.shape)
+print(actual.shape)
 
 
 
-
-
-
+plt.figure(figsize=(10,10))
+plt.plot(actual, label='actual')
+plt.plot(pred, label='prediction')
+plt.legend()
+plt.show()
